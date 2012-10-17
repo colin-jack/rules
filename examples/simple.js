761d8660efaa41647d29188cc926ee6cc01e68fd
Colin Jack <colin.jack@gmail.com>
2012-10-17 20:23:22 +0100

    Example.

--- examples/simple.js
+++ examples/simple.js
@@ -1,16 +1,16 @@
-require('./../umdDefine')
-var validatron = require('./../validatron');
-var mustBe = require('./../mustBe');
+var log = require('util').log,
+    inspect = require('util').inspect,
+    validatron = require('./../lib/validatron'),
+    mustBe = require('./../lib/mustBe');
 
-var veryInvalid = { name: "", "age" : -1, address: null, weight: 80 };
+var veryInvalid = { name: "", "age" : -1, address: null, weight: "bob" };
 
 var validationDefinition = {
     age: mustBe().populated().numeric({minimum : 0, maximum: 130}),
-    name: mustBe().populated().string({maxLength: 50}),
-    //address: mustBe().populated().
-    awesomeness: function() { this.populated().numeric(); }
+    name: mustBe().populated(), //.string({maxLength: 50}),
+    weight: function() { this.populated().numeric(); }
 }
 
 var result = validatron(veryInvalid, validationDefinition);
 
-require('util').log(result);
+log(inspect(result));
