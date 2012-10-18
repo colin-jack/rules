0be5c114abf22a441eeecb7e28d3c27cf35883df
Colin Jack <colin.jack@gmail.com>
2012-10-18 21:02:51 +0100

    More tests.

--- spec/validators/numericValidator/numericRangeValidator/failingToCreate.js
+++ spec/validators/numericValidator/numericRangeValidator/failingToCreate.js
@@ -1,37 +1,48 @@
 var assert = require('chai').assert;
 var numericRangeValidator = require('./../../../testFixture').require('numericRangeValidator')
 
-describe('When you try to create range validator', function() {
-    var createValidatorWrapper = function(config) {
-        return function() {
-            numericRangeValidator.create(config);
-        }
-    }
-
-    var shouldThrowErrorWhenCreated = function(minimum, maximum, expectedMessage) {
-        it('should throw exception', function() {
-            var config = { minimum : minimum, maximum : maximum };
-            assert.throws(createValidatorWrapper(config), Error, expectedMessage);
-        })
-    };
+describe('When you try to create numeric range validator', function() {
+    var MinMessage = /Minimum must be a number./;
+    var MaxMessage = /Maximum must be a number./;
 
     describe('but minimum is not a number', function() {
-        shouldThrowErrorWhenCreated('bob', 9, "Minimum must be a number.");
+        shouldThrowErrorWhenCreated('bob', 9, MinMessage);
+    });
+
+    describe('but minimum is null', function() {
+        shouldThrowErrorWhenCreated(null, 5, MinMessage);
     });
 
     describe('but minimum is NaN', function() {
-        shouldThrowErrorWhenCreated(NaN, 9, "Minimum must be a number.");
+        shouldThrowErrorWhenCreated(NaN, 9, MinMessage);
     });
 
     describe('but maximum is not a number', function() {
-        shouldThrowErrorWhenCreated(0, "bill", "Maximum must be a number.");
+        shouldThrowErrorWhenCreated(0, "bill", MaxMessage);
+    });
+
+    describe('but maximum is null', function() {
+        shouldThrowErrorWhenCreated(5, null, MaxMessage);
     });
 
     describe('but maximum is NaN', function() {
-        shouldThrowErrorWhenCreated(0, NaN, "Maximum must be a number.");
+        shouldThrowErrorWhenCreated(0, NaN, MaxMessage);
     });
 
     describe('but minimum is greater than maximum', function() {
         shouldThrowErrorWhenCreated(50, 40, "Minimum must be less than maximum.");
     });
+
+    function createValidatorWrapper(config) {
+        return function() {
+            numericRangeValidator.create(config);
+        }
+    }
+
+    function shouldThrowErrorWhenCreated(minimum, maximum, expectedMessage) {
+        it('should throw exception', function() {
+            var config = { minimum : minimum, maximum : maximum };
+            assert.throws(createValidatorWrapper(config), expectedMessage);
+        })
+    };
 });
--- spec/validators/string/length/failingToCreate.js
+++ spec/validators/string/length/failingToCreate.js
@@ -2,38 +2,41 @@ var assert = require('chai').assert;
 var stringLengthValidator = require('./../../../testFixture').require('stringLengthValidator')
 
 describe('When you try to create range validator', function() {
+    var MinLengthMessage = /Minimum length must be a number/;
+    var MaxLengthMessage = /Maximum length must be a number/;
+
     describe('but minimum length is not a number', function() {
-        shouldThrowErrorWhenCreated('bob', 9, "Minimum must be a number.");
+        shouldThrowErrorWhenCreated('bob', 9, MinLengthMessage);
     });
 
-    describe('but minimum is nulll', function() {
-        shouldThrowErrorWhenCreated(null, 9, "Minimum must be a number.");
+    describe('but minimum is null', function() {
+        shouldThrowErrorWhenCreated(null, 9, MinLengthMessage);
     });
 
     describe('but minimum length is NaN', function() {
-        shouldThrowErrorWhenCreated(NaN, 9, "Minimum must be a number.");
+        shouldThrowErrorWhenCreated(NaN, 9, MinLengthMessage);
     });
 
     describe('but maximum length is not a number', function() {
-        shouldThrowErrorWhenCreated(0, "bill", "Maximum must be a number.");
+        shouldThrowErrorWhenCreated(0, "bill", MaxLengthMessage);
     });
 
-    describe('but maximum is nulll', function() {
-        shouldThrowErrorWhenCreated(5, null, "Maximum must be a number.");
+    describe('but maximum is null', function() {
+        shouldThrowErrorWhenCreated(5, null, MaxLengthMessage);
     });
 
     describe('but maximum length is NaN', function() {
-        shouldThrowErrorWhenCreated(0, NaN, "Maximum must be a number.");
+        shouldThrowErrorWhenCreated(0, NaN, MaxLengthMessage);
     });
 
     describe('but minimum is greater than maximum', function() {
-        shouldThrowErrorWhenCreated(50, 40, "Minimum must be less than maximum.");
+        shouldThrowErrorWhenCreated(50, 40, /Minimum length must be less than maximum./);
     });
 
     function shouldThrowErrorWhenCreated(minLength, maxLength, expectedMessage) {
         it('should throw exception', function() {
             var config = { minLength : minLength, maxLength : maxLength };
-            assert.throws(createValidatorWrapper(config), Error, expectedMessage);
+            assert.throws(createValidatorWrapper(config), expectedMessage);
         })
     };
 
