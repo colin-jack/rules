cf2a04aa9c0ccfc257e982d4a2371a132b98c416
Colin Jack <colin.jack@gmail.com>
2012-10-21 20:55:05 +0100

    Renamed test folder.

--- spec/validators/date/range/now.js
+++ spec/validators/date/range/now.js
@@ -1,2 +1,47 @@
 // non-numeric value
 // in-appropriate key
+
+var assert = require('chai').assert;
+var now = lib.require('now');
+var moment = require('moment');
+
+describe('date range validator - now interface', function() {
+    describe('When you try to pass a boolean value to now.add', function() {
+        shouldThrowOnInvalidValuePassedToAdd(true)
+    });
+
+    describe('When you try to pass a boolean value to now.subtract', function() {
+        shouldThrowOnInvalidValuePassedToSubtract(true)
+    });
+
+    describe('When you try to pass an inappropriate key to now.add', function() {
+        shouldThrowOnInvalidKeyPassedToAdd("millenium")
+        shouldThrowOnInvalidKeyPassedToAdd("bob")
+        shouldThrowOnInvalidKeyPassedToAdd("")
+        shouldThrowOnInvalidKeyPassedToAdd(undefined)
+    });
+
+    function shouldThrowOnInvalidKeyPassedToAdd(key) {
+        it('should raise an exception', function() {
+            var passingInValue = function() { now.add(key, 1)}
+
+            assert.throws(passingInValue);
+        });
+    }
+
+    function shouldThrowOnInvalidValuePassedToAdd(value) {
+        it('should raise an exception', function() {
+            var passingInValue = function() { now.add("years", value)}
+
+            assert.throws(passingInValue);
+        });
+    }
+
+    function shouldThrowOnInvalidValuePassedToSubtract(value) {
+        it('should raise an exception', function() {
+            var passingInValue = function() { now.subtract("years", value)}
+
+            assert.throws(passingInValue);
+        });
+    }
+});
