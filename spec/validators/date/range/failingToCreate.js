6f89c14cadd3453c2789a81bc045d9a811417986
Colin Jack <colin.jack@gmail.com>
2012-10-21 20:15:07 +0100

    More tests.

--- spec/validators/date/range/failingToCreate.js
+++ spec/validators/date/range/failingToCreate.js
@@ -8,36 +8,61 @@ describe('date range validator', function() {
         var NotParsable = /When included the before\/after values should be dates or parsable as dates./;
 
         describe('but before is a number', function() {
-            shouldThrowErrorWhenCreated(5, moment(), NotParsable);
+            shouldThrowErrorWhenCreated(5, moment());
+        });
+
+        describe('but before is a Date', function() {
+            shouldThrowErrorWhenCreated(new Date(), moment());
+        });
+
+        describe('but before is a date as a string', function() {
+            shouldThrowErrorWhenCreated(new Date().toUTCString(), moment());
         });
 
         describe('but before is an array', function() {
-            shouldThrowErrorWhenCreated([2010, 11, 11], moment(), NotParsable);
+            shouldThrowErrorWhenCreated([2010, 11, 11], moment());
         })
 
         describe('but before is a boolean', function() {
-            shouldThrowErrorWhenCreated(moment(), moment(), NotParsable);
+            shouldThrowErrorWhenCreated(true, moment());
         });
 
-        // describe('but before is null', function() {
-        //     shouldThrowErrorWhenCreated(null, 5, NotParsable);
-        // });
+        describe('but before is null', function() {
+            shouldThrowErrorWhenCreated(null, moment());
+        });
 
-        // describe('but before is NaN', function() {
-        //     shouldThrowErrorWhenCreated(NaN, 9, NotParsable);
-        // });
+        describe('but before is NaN', function() {
+            shouldThrowErrorWhenCreated(NaN, moment());
+        });
 
-        // describe('but after is a number', function() {
-        //     shouldThrowErrorWhenCreated(0, "bill", NotParsable);
-        // });
+        describe('but after is a number', function() {
+            shouldThrowErrorWhenCreated(moment(),5 );
+        });
 
-        // describe('but after is null', function() {
-        //     shouldThrowErrorWhenCreated(5, null, NotParsable);
-        // });
+        describe('but after is an array', function() {
+            shouldThrowErrorWhenCreated(moment(), [2010, 11, 11]);
+        })
 
-        // describe('but after is NaN', function() {
-        //     shouldThrowErrorWhenCreated(0, NaN, NotParsable);
-        // });
+        describe('but after is a boolean', function() {
+            shouldThrowErrorWhenCreated(moment(), true);
+        });
+
+        describe('but after is null', function() {
+            shouldThrowErrorWhenCreated(moment(), null);
+        });
+
+        describe('but after is NaN', function() {
+            shouldThrowErrorWhenCreated(moment(), NaN);
+        });
+
+         describe('but after is a Date', function() {
+            shouldThrowErrorWhenCreated(moment(), new Date());
+        });
+
+        describe('but after is a date as a string', function() {
+            shouldThrowErrorWhenCreated(moment(), new Date().toUTCString());
+        });
+
 
         // describe('but minimum is greater than maximum', function() {
         //     shouldThrowErrorWhenCreated(50, 40, "Min must be less than max.");
@@ -53,13 +78,13 @@ describe('date range validator', function() {
             }
         }
 
-        function shouldThrowErrorWhenCreated(beforeValue, afterValue, expectedMessage) {
+        function shouldThrowErrorWhenCreated(beforeValue, afterValue) {
             it('should throw exception', function() {
                 debugger;
                 var beforeFunc = function() { return beforeValue; };
                 var afterFunc = function() { return afterValue; };
                 var config = { before : beforeFunc, after : afterFunc };
-                assert.throws(createValidatorWrapper(config), expectedMessage);
+                assert.throws(createValidatorWrapper(config), NotParsable);
             })
         };
     });
