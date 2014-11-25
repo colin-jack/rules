ac7a7124469e1dbe57e06c10be330d6c19eec72e
Colin Jack <colin.jack@gmail.com>
2014-11-25 19:38:48 +0000

    Slight fix.

--- spec/fluentInterface/ensure.js
+++ spec/fluentInterface/ensure.js
@@ -5,7 +5,7 @@ describe("rules - ensure: ", function() {
     describe("when you use ensure for integer validation", function() {
         var NotIntegerErrorType = "not_an_integer";
         var NotNumericErrorType = "not_numeric";
-
+        
         it("should fail if integer value is not numeric", function() {
             var shouldFail = function() { ensure(false).integer(); }
             assertThrows(shouldFail, undefined, "The value must be numeric.", NotNumericErrorType);
@@ -26,14 +26,9 @@ describe("rules - ensure: ", function() {
             assertThrows(shouldFail, undefined, "The value must be an integer.", NotIntegerErrorType);
         });
         
-        it("should fail if integer value is not an integer", function () {
-            var shouldFail = function () { ensure(5.5).integer(); }
-            assertThrows(shouldFail, undefined, "The value must be an integer.", NotIntegerErrorType);
-        });
-
-        it("should pass if integer value is OK", function() {
-            var shouldPass = function() { ensure(5, "age").integer(); }
-            assert.doesNotThrow(shouldPass);
+        it("should fail if integer value is an integer as a string", function () {
+            var shouldFail = function () { ensure("5", "age").integer(); }
+            assertThrows(shouldFail, "age", "The 'age' value must be an integer.", NotIntegerErrorType);
         });
     });
 
@@ -49,13 +44,11 @@ describe("rules - ensure: ", function() {
         });
 
         it("should fail if integer value is not a string and include name in the message", function () {
-            debugger;
             var shouldFail = function() { ensure(false, "name").populated().string(); }
             assertThrows(shouldFail, "name", "The 'name' value must be a string.", "not_a_string");
         });
         
         it("should fail if integer value is not a string", function () {
-            debugger;
             var shouldFail = function () { ensure(false).populated().string(); }
             assertThrows(shouldFail, undefined, "The value must be a string.", "not_a_string");
         });
